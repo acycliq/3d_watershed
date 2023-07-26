d2c54fb40fea8e2c1c2bc25cb69018cfa44c32d7
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-07-26 19:52:20 +0100

    first commit

--- segment.py
+++ segment.py
@@ -10,6 +10,8 @@ import fastremap
 from scipy import ndimage
 import diplib as dip
 from utils import colourise, unpack
+from scipy.sparse import coo_matrix, csr_matrix, csc_matrix
+from base_logger import logger
 
 
 def intersection_over_union(masks_true, masks_pred):
@@ -30,11 +32,47 @@ def intersection_over_union(masks_true, masks_pred):
         matrix of IOU pairs of size [x.max()+1, y.max()+1]
 
     """
+
     overlap = _label_overlap(masks_true, masks_pred)
     n_pixels_pred = np.sum(overlap, axis=0, keepdims=True)
     n_pixels_true = np.sum(overlap, axis=1, keepdims=True)
     iou = overlap / (n_pixels_pred + n_pixels_true - overlap)
     iou[np.isnan(iou)] = 0.0
+    return iou
+
+
+def intersection_over_union_2(masks_true, masks_pred):
+    """ intersection over union of all mask pairs
+
+    Parameters
+    ------------
+
+    masks_true: ND-array, int
+        ground truth masks, where 0=NO masks; 1,2... are mask labels
+    masks_pred: ND-array, int
+        predicted masks, where 0=NO masks; 1,2... are mask labels
+
+    Returns
+    ------------
+
+    iou: ND-array, float
+        matrix of IOU pairs of size [x.max()+1, y.max()+1]
+
+    """
+    overlap = _label_overlap2(masks_true, masks_pred)
+
+    # print('x.max is %d' % masks_true.max())
+    # print('y.max is %d' % masks_pred.max())
+    n_pixels_pred = overlap.sum(axis=0)
+    n_pixels_true = overlap.sum(axis=1)
+
+    x = np.array((n_pixels_true)).flatten()
+    y = np.array((n_pixels_pred)).flatten()
+
+    pred_plus_true = x[overlap.row] + y[overlap.col]
+    iou_data = overlap.data /(pred_plus_true - overlap.data)
+    iou = coo_matrix((iou_data, (overlap.row, overlap.col)), shape=overlap.shape)
+
     return iou
 
 
@@ -65,12 +103,43 @@ def _label_overlap(x, y):
     return overlap
 
 
+# @jit(nopython=True)
+def _label_overlap2(x, y):
+    """ fast function to get pixel overlaps between masks in x and y
+
+    Parameters
+    ------------
+
+    x: ND-array, int
+        where 0=NO masks; 1,2... are mask labels
+    y: ND-array, int
+        where 0=NO masks; 1,2... are mask labels
+
+    Returns
+    ------------
+
+    overlap: ND-array, int
+        matrix of pixel overlaps of size [x.max()+1, y.max()+1]
+
+    """
+    x = x.ravel()
+    y = y.ravel()
+    data = np.ones(len(x))
+    n_rows = 1 + x.max().astype(np.int32)
+    n_cols = 1 + y.max().astype(np.int32)
+    overlap = coo_matrix((data, (x, y)), shape = (n_rows, n_cols), dtype=np.int32)
+    # overlap = coo_matrix((1 + x.max().astype(np.int32), 1 + y.max().astype(np.int32)), dtype=np.int32 )
+    # overlap.data[x, y] += 1
+    overlap = overlap.tocsc().tocoo()
+    return overlap
+
+
 def stitch3D(masks, stitch_threshold=0.25):
     """ stitch 2D masks into 3D volume with stitch_threshold on IOU """
     mmax = masks[0].max()
     for i in range(len(masks)-1):
-        print('stitching mask %d ' % i)
-        iou = intersection_over_union(masks[i+1], masks[i])[1:,1:]
+        logger.info('stitching mask %d ' % i)
+        iou = intersection_over_union(masks[i + 1], masks[i])[1:, 1:]
         if iou.size > 0:
             iou[iou < stitch_threshold] = 0.0
             # iou[iou < iou.max(axis=0)] = 0.0
@@ -83,13 +152,46 @@ def stitch3D(masks, stitch_threshold=0.25):
     return masks
 
 
-def remove_small_cells(cell_labels, min_size=5):
+def stitch3D_coo(masks, stitch_threshold=0.25):
+    """ stitch 2D masks into 3D volume with stitch_threshold on IOU """
+    mmax = masks[0].max()
+    for i in range(len(masks)-1):
+        logger.info('stitching mask %d ' % i)
+        iou_coo = intersection_over_union_2(masks[i+1], masks[i])
+        # iou = iou_coo.toarray()[1:, 1:]
+
+        # remove now first column and first row from the coo matrix
+        is_coord_zero = iou_coo.row * iou_coo.col
+        row = iou_coo.row[is_coord_zero != 0] - 1
+        col = iou_coo.col[is_coord_zero != 0] - 1
+        data = iou_coo.data[is_coord_zero != 0]
+        m,n = iou_coo.shape
+        iou_coo = coo_matrix((data, (row, col)), shape=(m-1, n-1))
+        if iou_coo.data.size > 0:
+            # remove elements smaller than the threshold
+            idx = iou_coo.data >= stitch_threshold
+            iou_coo.data = iou_coo.data[idx]
+            iou_coo.col = iou_coo.col[idx]
+            iou_coo.row = iou_coo.row[idx]
+
+            # iou[iou < iou.max(axis=0)] = 0.0
+            istitch = iou_coo.argmax(axis=1) + 1
+            istitch = np.asarray(istitch).flatten()
+            ino = np.nonzero(iou_coo.max(axis=1)==0.0)[0]
+            istitch[ino] = np.arange(mmax+1, mmax+len(ino)+1, 1, int)
+            mmax += len(ino)
+            istitch = np.append(np.array(0), istitch)
+            masks[i+1] = istitch[masks[i+1]]
+    return masks
+
+
+def remove_small_cells(i, cell_labels, min_size=5):
     # p_cut = 2
     regions = pd.DataFrame(regionprops_table(cell_labels, properties=['label', 'area']))
     # min_size = np.percentile(regions.area, p_cut)
     # logger.info("the %dth percentile is: %d" % (p_cut, min_size))
     small_cells = regions[regions.area <= min_size]
-    print("found %d cells with area less than %d" % (small_cells.shape[0], min_size))
+    logger.info("image: %d: Found %d cells with area less than %d" % (i, small_cells.shape[0], min_size))
 
     # remove small cells
     cell_labels = fastremap.mask(cell_labels, small_cells.label.values)
@@ -97,14 +199,14 @@ def remove_small_cells(cell_labels, min_size=5):
     return cell_labels.astype(np.uint64), label_map
 
 
-def watershed(bw_img):
-    Image.fromarray(bw_img).save(r'.\debug\bw_img.jpg')
+def watershed(i, bw_img):
+    Image.fromarray(bw_img).save(r'.\microglia\debug\bw_img_%03d.jpg' % i)
 
     # shrink the shapes by a few pixels
     s = generate_binary_structure(2, 1)
     bw_eroded = morphology.erosion(bw_img == 255, s)
     bw_eroded = bw_eroded.astype(np.uint8)
-    Image.fromarray(bw_img).save(r'.\debug\bw_eroded.jpg')
+    Image.fromarray(bw_img).save(r'.\microglia\debug\bw_eroded_%03d.jpg' % i)
 
     # for each shape get the distance from the closest zero-valued pixel
     distance = ndimage.distance_transform_edt(bw_eroded)
@@ -114,49 +216,55 @@ def watershed(bw_img):
 
     # seeds: the brightest pixels of the shapes, typically close to the center of each shape.
     seeds = dip.Maxima(distance)
-    cell_labels = dip.SeededWatershed(-distance, seeds, dilated_img > 0,
+    cell_labels = dip.SeededWatershed(dip.Gauss(-distance), seeds, dilated_img > 0,
                                       maxDepth=distance.max(),
                                       flags={'labels'}
                                       )
-    print('max distance %f' % distance.max())
+    # logger.info('max distance %f' % distance.max())
     cell_labels = np.array(cell_labels)
-    cell_labels, _ = remove_small_cells(cell_labels)
+    cell_labels, _ = remove_small_cells(i, cell_labels)
     return cell_labels
 
 
 def app(masks_url, background_url):
-    background = skimage.io.imread(background_url)
-    background = background[20:40]
+    image_3d = skimage.io.imread(background_url)
+    # image_3d = image_3d[20:30]
 
-    array_3d = skimage.io.imread(masks_url)
-    array_3d = array_3d[20:40]
+    bw_masks = skimage.io.imread(masks_url)
+    # bw_masks = bw_masks[20:30]
 
     ## for debugging use
     # background = background[21:23, 4024:4247, 1952:2226]
     # array_3d = array_3d[21:23, 4024:4247, 1952:2226]
 
     labels_list = []
-    for i, img in enumerate(array_3d):
-        lbl = watershed(img)
+    for i, img in enumerate(bw_masks):
+        lbl = watershed(i, img)
         labels_list.append(lbl)
     labels = np.stack(labels_list)
 
-    stitched_labels = stitch3D(labels.astype(np.uint64), stitch_threshold=0.009)
-    rgb_masks = colourise(stitched_labels, background)
-    unpack(rgb_masks, r"microglia_backround_images\mask_tiles_stitched_fixed_v4", mode="RGB", make_tiles=True)
+    # logger.info('stitch3D starts')
+    # stitched_labels = stitch3D(labels.astype(np.uint64), stitch_threshold=0.009)
+    # logger.info('stitch3D finishes')
 
-    out_npy = r'microglia_backround_images\mask_tiles_stitched_fixed_v4\stitched_masks.npy'
-    np.save(out_npy, stitched_labels)
-    print('stitched_masks saved at %s' % out_npy)
+    logger.info('stitch3D_coo starts')
+    stitched_labels_2 = stitch3D_coo(labels.astype(np.uint64), stitch_threshold=0.009)
+    logger.info('stitch3D_coo startsfinishes')
+
+    out_npy = r'.\microglia\stitched_masks.npy'
+    np.save(out_npy, stitched_labels_2)
+    logger.info('stitched_masks saved at %s' % out_npy)
+
+    rgb_masks = colourise(stitched_labels_2, image_3d)
+    unpack(rgb_masks, r".\microglia", mode="RGB", make_tiles=False)
     # Image.fromarray(rgb_masks[10].astype(np.uint8), "RGB")
-    print("Done!")
-
+    logger.info("Done!")
 
     # array_3d = array_3d.transpose(2, 0, 1)
 
 if __name__ == "__main__":
     # page_list = [46, 47, 48]
-    masks_tif = "all_pages.tiff"  # black and white image
-    all_img_adj_tiff = 'all_img_adj.tiff'  # the background after all the adjustments
-    app(masks_tif, all_img_adj_tiff)
+    bw_masks_tif = r".\microglia\bw_image.tiff"  # black and white image
+    image_3d_tiff = r'.\microglia\adj_img.tiff'  # the background after all the adjustments
+    app(bw_masks_tif, image_3d_tiff)
 
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import os
+import cv2
 import pciSeq
 import numpy as np
 from PIL import Image
 from skimage.color import label2rgb
+from skimage.util import img_as_ubyte
+from skimage import morphology, restoration
 
 
 def rgb2hex(rgb):
@@ -104,3 +107,102 @@ def unpack(stack, out_dir, mode=None, make_tiles=False):
             print("tiles created at was saved at %s" % tiles_dir)
         else:
             print("Image %s is totally empty. Skipping the tile maker..." % fName)
+
+
+def adjust_image(img, n):
+    '''
+    takes in an image (grayscale, uint8) and adds some contrast.
+    Returns an array uint8
+    :param img_url:
+    :return:
+    '''
+    img = normalize_img(img)
+    lims = _stretchlim(img)
+    dapi_adj = _imadjust2(img, lims)
+
+    print("rolling_ball")
+    background = restoration.rolling_ball(dapi_adj)
+    Image.fromarray(background.astype(np.uint8)).save("background_%d.jpg" % n)
+    dapi_restored = dapi_adj - background
+    # Image.fromarray(dapi_restored.astype(np.uint8)).save("dapi_restored.tif")
+
+    print("adaptiveThreshold")
+    mask = cv2.adaptiveThreshold(dapi_restored.astype(np.uint8),
+                                 255,
+                                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY,
+                                 101,
+                                 0)
+    return mask, dapi_adj
+
+
+def normalize_img(img, normalize=[0, 99.9]):
+    perc1, perc2 = np.percentile(img, list(normalize))
+    img = img - perc1
+    img /= (perc2-perc1)
+    img = img * 255.0
+    img = np.clip(img, 0, 255)
+    img = img.astype(np.uint8)
+    return img
+
+
+def _stretchlim(img):
+    # adjust contrast
+    nbins = 255
+    tol_low = 0.01
+    tol_high = 0.99
+    sz = np.shape(img)
+    if len(sz) == 2:
+        img = img[:, :, None]
+        sz = np.shape(img)
+
+    p = sz[2]
+    ilowhigh = np.zeros([2, p])
+    for i in range(0, p):
+        hist, bins = np.histogram(img[:, :, i].ravel(), nbins + 1, [0, nbins])
+        cdf = np.cumsum(hist) / sum(hist)
+        ilow = np.argmax(cdf > tol_low)
+        ihigh = np.argmax(cdf >= tol_high)
+        if ilow == ihigh:
+            ilowhigh[:, i] = np.array([1, nbins])
+        else:
+            ilowhigh[:, i] = np.array([ilow, ihigh])
+
+    lims = ilowhigh / nbins
+    return lims
+
+
+def _imadjust2(img, lims):
+    # adjusts intensity
+    lims = lims.flatten()
+    img2 = np.copy(img)
+    lowIn = lims[0]
+    highIn = lims[1]
+    lowOut = 0
+    highOut = 1
+    gamma = 1
+    lut = _adjustWithLUT(lowIn, highIn, lowOut, highOut, gamma)
+    return lut[img2].astype(np.uint8)
+
+def _adjustWithLUT(lowIn, highIn, lowOut, highOut, gamma):
+    lutLength = 256  # assumes uint8
+    lut = np.linspace(0, 1, lutLength)
+    lut = _adjustArray(lut, lowIn, highIn, lowOut, highOut, gamma)
+    lut = _img_as_ubyte(lut)
+    return lut
+
+def _adjustArray(img, lIn, hIn, lOut, hOut, g):
+    # %make sure img is in the range [lIn;hIn]
+    img = np.maximum(lIn, np.minimum(hIn, img));
+
+    out = ((img - lIn) / (hIn - lIn)) ** g
+    out = out ** (hOut - lOut) + lOut
+    return out
+
+
+def _img_as_ubyte(x):
+    out = np.zeros(x.shape)
+    out[x == 0.3] = 77
+    out[x != 0.3] = img_as_ubyte(x)[x != 0.3]
+    return out
+

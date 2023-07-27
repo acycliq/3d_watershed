1542ca408d88595c500ca60f6e89658dad260649
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-07-27 18:36:34 +0100

    clean up

--- label_spots.py
+++ label_spots.py
@@ -1,38 +1,18 @@
-from scipy.sparse import coo_matrix, csr_matrix
+from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
 from base_logger import logger
-from multiprocessing import Pool, cpu_count
+from multiprocessing import cpu_count
 from multiprocessing.dummy import Pool as ThreadPool
 
-def app():
-    spots_url = r"F:\data\Christina\3D_watershed\spots_WT997_icvAB_OMP_restitched.csv"
-    spots = pd.read_csv(spots_url)
-    print(spots.shape)
 
-    masks = np.load(r".\microglia\stitched_masks.npy")
-
+def label_spots(spots, masks):
     z_stacks = np.unique(spots.z_stack)
-    temp = []
-    for z in z_stacks:
-        df = spots[spots.z_stack == z].copy()
-        mask = masks[int(np.floor(z))]
-        mask_csr = csr_matrix(mask)
-        logger.info('doing %d' % z)
-        label = mask_csr[df.y, df.x]
-        label = np.asarray(label)[0]
-        df['label'] = label
-        temp.append(df)
 
-    out = np.vstack(temp)
-    logger.info(out.shape)
-
-    logger.info('Start par')
     pool = ThreadPool(cpu_count())
     out = pool.map(loop_body(spots, masks), z_stacks)
     out = pd.concat(out)
-    logger.info('end par')
-    logger.info('ok')
+    logger.info(out.shape)
     return out
 
 
@@ -48,5 +28,9 @@ def loop_body(spots, masks):
         return df
     return inner_fun
 
+
 if __name__ == "__main__":
-    app()
+    spots_url = r"F:\data\Christina\3D_watershed\spots_WT997_icvAB_OMP_restitched.csv"
+    spots_df = pd.read_csv(spots_url)
+    masks = np.load(r".\microglia\stitched_masks.npy")
+    label_spots(spots_df, masks)

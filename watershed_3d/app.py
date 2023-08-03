4b1ae8a51331efa195efa810e49491eb0032b051
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-08-03 23:12:14 +0100

    docstring

--- watershed_3d/app.py
+++ watershed_3d/app.py
@@ -7,6 +7,19 @@ from watershed_3d.base_logger import logger
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def main(opts):
+    """
+    Main entry point for 3d watershed segmentation
+    :param opts: Dictionary with the following keys:
+        'do_rolling_ball': Boolean, if trye then the image will be filtered by the rolling-ball
+                            algorithm to correcct for uneven illumination/exposure. Use that on
+                            extreme cases as it increases execution time massively
+        'microglia_image': Path to your 3d image to be segmented with watershed. Format must be ZYX
+        'exclude_pages':    List of integers denoting the pages to be excluded from the segmentation.
+
+    :return: An array of the same size as your image with the segmentation masks. Pages that
+    have been excluded are totally black, iw all values are zero
+    """
+
     bw_arr, adj_arr = stack_to_images(opts)
     out = segment.main(bw_arr, adj_arr, opts)
     return out

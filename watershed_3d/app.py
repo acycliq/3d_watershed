4587c2b30c88efef0599dde09825c644ed5f4afb
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-11-11 23:12:08 +0000

    updated docstring

--- watershed_3d/app.py
+++ watershed_3d/app.py
@@ -8,33 +8,47 @@ from watershed_3d.base_logger import logger
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def app(image_url=None, exclude_pages=[], do_rolling_ball=False, mode='2d_stitch', maxDepth=1.0):
+def app(image_url=None, cfg=None):
     """
     Main entry point for 3d watershed segmentation
     image_url: Path to your 3d image to be segmented with watershed. Format must be ZYX
-    do_rolling_ball: Boolean, if true then the image will be processed with the rolling-ball
-                        algorithm to correct for uneven illumination/exposure. Use that on
-                        extreme cases as it increases execution time massively
-    exclude_pages:    List of integers denoting the pages to be excluded from the segmentation.
-    mode: Should be either '2d_stitch' or '3d'
-    'maxDepth': parameter that controls whether two shapes when their boundaries meet, will be
-                merged or not. Is it is set too high then you will end up with an undersegmented
-                image (few and very large shapes). If it is too low, then you will have an
-                oversegmented image (too many and probably small shapes). maxDepth is only
-                relevant if mode='3d', otherwise it is ignored.
+    cfg: dictionary wth user-defined options. The keys of the dict can be:
+        'do_rolling_ball': Boolean, if true then the image will be processed with the rolling-ball
+                            algorithm to correct for uneven illumination/exposure. Use that on
+                            extreme cases as it increases execution time massively
+        'exclude_pages':    List of integers denoting the pages to be excluded from the segmentation.
+        'mode': Should be either '2d_stitch' or '3d'
+        'stitch_threshold':
+        'maxDepth': parameter that controls whether two shapes when their boundaries meet, will be
+                    merged or not. Is it is set too high then you will end up with an undersegmented
+                    image (few and very large shapes). If it is too low, then you will have an
+                    oversegmented image (too many and probably small shapes). maxDepth is only
+                    relevant if mode='3d', otherwise it is ignored.
 
 
     returns an array with the segmentation masks. It has same shape as your 3d image. Pages that
     have been excluded are totally black, ie all values are zero
     """
+
+    if cfg is None:
+        cfg = {
+            'exclude_pages': [], 'do_rolling_ball': False, 'mode': '2d_stitch', 'stitch_threshold': 0.009,
+            'maxDepth': 1.0
+        }
     assert image_url is not None, "Need to pass the path to your image when you call app()"
 
-    opts = {'microglia_image': image_url,
-            'exclude_pages': exclude_pages,
-            'do_rolling_ball': do_rolling_ball,
-            'mode': mode,
-            'maxDepth': maxDepth
+    cfg = {} if cfg is None else cfg
+
+    # dict with default options
+    opts = {'exclude_pages': [],
+            'do_rolling_ball': False,
+            'mode': '2d_stitch',
+            'stitch_threshold': 0.009,
+            'maxDepth': 1.0
             }
+
+    # update with user defined options
+    opts.update(cfg)
 
     bw_arr, adj_arr = preprocess(opts)
     if mode == '2d_stitch':
